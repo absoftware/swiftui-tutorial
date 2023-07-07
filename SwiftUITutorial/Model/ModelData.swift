9a224b490c331a2d7116069176b56738409e1d8f
Ariel Bogdziewicz <ariel.bogdziewicz@gmail.com>
2023-07-07 15:31:40 +0100

    Animating views and transitions.

--- SwiftUITutorial/Model/ModelData.swift
+++ SwiftUITutorial/Model/ModelData.swift
@@ -10,6 +10,7 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("LandmarkData.json")
+    var hikes: [Hike] = load("HikeData.json")
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
