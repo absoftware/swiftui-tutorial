46e56235a5b1e3b1a8b58309a7e94fbe66a550b3
Ariel Bogdziewicz <ariel.bogdziewicz@gmail.com>
2023-07-07 12:16:51 +0100

    Initialised SwiftUI project for iOS.

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Ariel Bogdziewicz on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
