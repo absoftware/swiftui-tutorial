c16d7a42dda0bbb0c191c71426bcbef1a9b23860
Ariel Bogdziewicz <ariel.bogdziewicz@gmail.com>
2023-07-07 15:09:40 +0100

    Drawing paths and shapes.

---
+++ SwiftUITutorial/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUITutorial
+//
+//  Created by Ariel Bogdziewicz on 07/07/2023.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
